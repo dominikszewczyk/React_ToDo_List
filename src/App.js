03bf8ec9b9c96eabd3e9f089ebd6719ab6e3135f
Dominik Szewczyk <dominikszewczyklim@gmail.com>
2024-02-29 16:24:13 +0100

    Clearing the project - deleting HTML/CSS templete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import ToDoForm from './components/ToDoForm';
+import './components/ToDoHeader';
+import ToDoHeader from './components/ToDoHeader';
+import ToDoList from './components/ToDoList';
+import ToDoWrapper from './components/ToDoWrapper';
+
+const toDoItems = [
+  {id: 0, task: 'Finalize React course'},
+  {id: 1, task: 'Start React Hook course'},
+  {id: 2, task: 'Crate react "To Do list" App'},
+  {id: 3, task: "One more"}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <ToDoHeader header="My To Do App" />
+
+      <ToDoWrapper>
+        <ToDoForm />
+      </ToDoWrapper>
+
+      <ToDoWrapper>
+        <ToDoList items={ toDoItems }/>
+      </ToDoWrapper>
     </div>
   );
 }

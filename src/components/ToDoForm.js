42868016b6a713efa0de06e8c0839ce1190e0801
Dominik Szewczyk <dominikszewczyklim@gmail.com>
2024-03-01 15:13:58 +0100

    Adding new items to list and displaying items

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import { React, useState } from 'react'
 
-export default function ToDoForm() {
+export default function ToDoForm( props) {
+    const [newItem, setNewItem] = useState("")
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (newItem === "") 
+            return
+        props.onSubmit(newItem);
+        setNewItem("");
+    };
 
-  return (
-    <form className="todo-form" onSubmit={ handleSubmit }>
-        <input 
-        type="text" 
-        className="todo-form__input" 
-        placeholder="Add new item"/>
-        <button 
-        type="submit" 
-        className="todo-form__button">add</button>
-    </form>
-  )
+    function handleInputChange(event) {
+        setNewItem(event.target.value)
+    };
+
+    return (
+        <>
+            <form className="todo-form" onSubmit={ handleSubmit }>
+                <label htmlFor="item">New item</label>
+                <input
+                    type="text"
+                    id="item"
+                    className="todo-form__input" 
+                    value={newItem}
+                    onChange={event => handleInputChange(event)}
+                    placeholder="Add new item"/>
+                <button 
+                    type="submit" 
+                    className="todo-form__button">add</button>
+            </form>
+        </>
+    )
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,10 +3,21 @@ import React from 'react'
 export default function ToDoList({ items }) {
 
   const listItems = items.map(item =>
-    <li className="todo-list__item">{ item.task }</li>
+    <li className="todo-list__item">
+      <label>
+        <input 
+          type="checkbox" 
+          disabled={ item.competed }
+        />
+        { item.title }
+      </label>
+    </li>
   );
   
   return (
-    <ul className="todo-list">{ listItems }</ul>
+    <>
+      {/* Add code if items list is empty - items.height === 0 */}
+      <ul className="todo-list">{ listItems }</ul>
+    </>
   )
 }
